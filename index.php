e2f212925876b0a52a2765f72ee2cc6b548781c1
saptak <deeps.sengupta@gmail.com>
2016-09-04 20:02:04 +0530

    commiting the update as well as the xml file for voice control

--- index.php
+++ index.php
@@ -1,10 +1,25 @@
 <?php 
 	
-	$sid = '';
-	$token = '';
-	$url = '';
+	$sid = 'ACad51599515c5bf6e068467f533c745d9';
+	$token = '337028e142cf0afe8b393cbbe60acc49';
+	$url = 'https://api.twilio.com/2010-04-01/Accounts/ACad51599515c5bf6e068467f533c745d9/Calls.json';
 
-	public function hit_curl(){
+	hit_curl($url);
 
+
+
+	function hit_curl($url){
+		$curl = curl_init();
+    	curl_setopt($curl, CURLOPT_URL,$url);
+    	curl_setopt($curl, CURLOPT_POST, 1);
+    	curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false); 
+    	curl_setopt($curl, CURLOPT_POSTFIELDS, "Url=http://localhost/twilio/voice.xml&To=%2B918013778491&From=%2B13342698968");
+    	curl_setopt($curl, CURLOPT_USERPWD, "ACad51599515c5bf6e068467f533c745d9:337028e142cf0afe8b393cbbe60acc49");
+    	curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+
+    	$result = curl_exec ($curl);
+    	echo curl_error($curl);
+    	curl_close ($curl);
+    	print $result;
 	}
 ?>
