907e054b1d2b2552f8ff1d1b13a67a37203e69e4
saptak <deeps.sengupta@gmail.com>
2016-09-04 20:08:11 +0530

    updated the changes

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
     	curl_setopt($curl, CURLOPT_URL,$url);
     	curl_setopt($curl, CURLOPT_POST, 1);
     	curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false); 
-    	curl_setopt($curl, CURLOPT_POSTFIELDS, "Url=http://localhost/twilio/voice.xml&To=%2B918013778491&From=%2B13342698968");
+    	curl_setopt($curl, CURLOPT_POSTFIELDS, "Url=https://github.com/saptaksengupta/twilio/blob/master/voice.xml&To=%2B918013778491&From=%2B13342698968");
     	curl_setopt($curl, CURLOPT_USERPWD, "ACad51599515c5bf6e068467f533c745d9:337028e142cf0afe8b393cbbe60acc49");
     	curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
 
